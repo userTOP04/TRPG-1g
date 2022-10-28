ca2b4efd67999a69aa2c388da3ee43aa4460176a
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2022-10-28 17:49:05 +0300

    Add files via upload

---
+++ shop.py
@@ -0,0 +1,16 @@
+def shop(player_name, player_maney, player_hp, player_xp, player_pt):
+    os.system("cls")
+    print(f"имя: {player_name}")
+    print(f"деньги: {player_maney}")
+    print(f"жизни: {player_hp}")
+    print(f"опыт: {player_xp}")
+    print(f"зелья: {player_pt}")
+
+
+    print(f"{player_name} приехал в лавку")
+
+
+    print("1 - Купить зелье за 10 монет")
+    print("2 - Уехать к камню")
+
+    input("+++++++++++")
