425d4242929b4d72f9300b644727fee624441d83
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2022-11-08 17:50:34 +0300

    Add files via upload

--- shop.py
+++ shop.py
@@ -1,37 +1,44 @@
+
 import os
 
 
 
-def shop(player_name, player_maney, player_hp, player_xp, player_pt):
+def shop(player):
+
+    name = player[0]
+    hp = player[2]
+    money = player[1]
+    xp = player[3]
+    pt = player[4]
+
+
     while True:
+
         os.system("cls")
-        print(f"имя: {player_name}")
-        print(f"деньги: {player_maney}")
-        print(f"жизни: {player_hp}")
-        print(f"опыт: {player_xp}")
-        print(f"зелья: {player_pt}")
+        print(f"имя", name)
+        print(f"жизьни", hp)
+        print(f"деньги", money)
+        print(f"опыт", xp)
+        print(f"зелья", pt)
 
-
-        print(f"{player_name} приехал в лавку")
+        print(f"{name} приехал в лавку")
 
 
         print("1 - Купить зелье за 10 монет")
         print("2 - Уехать к камню")
 
+
         answer = input("Введте номер ответа и нажмите ENTER")
+
+
         if answer == "1":
             os.system("cls")
-            if player_maney >= 10:
-                player_maney -= 10
-                player_pt += 1
+            if money >= 10:
+                money -= 10
+                pt += 1
             else:
                 print("недостадочно средств пшол вон")
-            input("Введте номер ответа и нажмите ENTER")
-
-
-
-
-
+            input("нажмите ENTER")
 
         elif answer == "2":
-            break
+            return (name, hp, money, xp, pt)
